884588e0f035d3eac60fa35f8ee73e6510f96fa4
piotrb91 <piotrekb91@wp.pl>
2017-07-24 22:34:33 +0200

    Added ShopContext

--- OnlineShop/OnlineShop/DAL/ShopContext.cs
+++ OnlineShop/OnlineShop/DAL/ShopContext.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,21 @@ namespace OnlineShop.DAL
 {
     public class ShopContext : DbContext
     {
+        public ShopContext() : base("ShopContext")
+        {
+
+        }
+
         public DbSet<Shop> Produkty {get; set;}
         public DbSet<Category> Categories { get; set; }
         public DbSet<Orders> Orders { get; set; }
         public DbSet<OrderPosition> OrderPositions { get; set; }
 
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
