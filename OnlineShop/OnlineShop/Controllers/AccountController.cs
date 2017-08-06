f99d7e7a530aba62d5cf06ce85f070c38e56cf3d
piotrb91 <piotrekb91@wp.pl>
2017-08-06 16:09:00 +0200

    Added login

---
+++ OnlineShop/OnlineShop/Controllers/AccountController.cs
@@ -0,0 +1,30 @@
+﻿using OnlineShop.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShop.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login(string returnUrl)
+        {
+
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+            else
+                return RedirectToAction("Index", "Home");
+        }
+
+    }
+}
